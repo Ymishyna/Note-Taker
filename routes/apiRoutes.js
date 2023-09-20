12b2227d8c81aaf5425ef4228e8e690ad361ebdc
Yana Mishyna <yana.mishina.92@gmail.com>
2023-09-20 14:18:54 -0400

    restractured my files, created folders for routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,6 @@
+// Dependencies 
+const path = require('path');
+const fs = require('fs')
+// Allows all notes to have a unique ID
+var uniqid = require('uniqid');
+
--- server.js
+++ server.js
@@ -1,45 +1,30 @@
 // Dependencies
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const db = require('./db/db.json')
-//Allows all notes to have a unique ID
-var uniqid = require('uniqid'); 
 
-
-// Create an express server
+// Creating an express server
 const app = express();
 
 // Creating environment variable port
 const PORT = process.env.PORT || 3001;
 
-
+// Link to assets
+app.use(express.static('public'));
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // Parse incoming JSON data
 app.use(express.json());
 
-// Link to assets
-app.use(express.static('public'));
+// Routes files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-// Creating routes
-// GET /notes should return the notes.html file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
 
-// GET * should return the index.html file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
 
-  // GET /api/notes should read the db.json file and return all saved notes as JSON
-  app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
-  });
+
+
 
 
 // Listener
 app.listen(PORT, () => {
-    console.log(`Server available at localhost${PORT}`);
+  console.log(`Server available at localhost${PORT}`);
 });
