3a660c5c4bcc1cfc7a56421de2e6188167cfa270
Yana Mishyna <yana.mishina.92@gmail.com>
2023-09-20 11:27:26 -0400

    added couple dependencies and started creating GET req for server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require("./db/db.json")
+const db = require('./db/db.json')
+//Allows all notes to have a unique ID
+var uniqid = require('uniqid'); 
 
 
 // Create an express server
@@ -20,11 +22,24 @@ app.use(express.json());
 // Link to assets
 app.use(express.static('public'));
 
+// Creating routes
+// GET /notes should return the notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
+// GET * should return the index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
+  // GET /api/notes should read the db.json file and return all saved notes as JSON
+  app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+  });
 
 
 // Listener
 app.listen(PORT, () => {
     console.log(`Server available at localhost${PORT}`);
-  });
+});
